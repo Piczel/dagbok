d224f6ffe21645d53425f670a8d68cd73464b7e8
Oskar Nylin <oskar.nylin@learnet.se>
2018-09-07 08:50:18 +0200

    Server response

---
+++ ajax/register.php
@@ -0,0 +1,28 @@
+<?php
+    $input = json_decode(file_get_contents("php://input"), true);    //Receives json input from client
+    $settings = parse_ini_file("settings.ini", true);      //Parses db settings from file
+    
+    //Creates a connection
+    $mysql = $settings["mysql"];    
+    $connection = new mysqli($mysql["host"], $mysql["username"], $mysql["password"], $mysql["dbname"]);
+    
+    $sql = "INSERT INTO account (forename, surname, email, `password`) VALUES ('".$input["forename"]."','".$input["surname"]."','".$input["email"]."','".$input["password"]."')";
+
+    //echo $sql."<br>";
+
+    if($connection->multi_query($sql) === true){
+    //Query succeeded
+        $response = [
+            "status"=>true,
+            "message"=>"kontot registrerades"
+        ];
+    }else{
+    //Query failed    
+        $response = [
+            "status"=>false,
+            "message"=>"kontot registrerades inte"
+        ];
+
+    }
+    echo json_encode($response);    //Converts php-array to json and prints response 
+?>
