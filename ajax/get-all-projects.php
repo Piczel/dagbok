980bd9d2694f39bf1a20e9081bca1f5a7e11e3a8
Oskar Nylin <oskar.nylin@learnet.se>
2018-09-12 09:49:20 +0200

    get-all-projects

--- ajax/get-all-projects.php
+++ ajax/get-all-projects.php
@@ -1,1 +1,39 @@
-TESTING
+<?php
+    $input = json_decode(file_get_contents("php://input"), true);   //Receives json input from client
+    try {
+        if(!isset($_SESSION["signedInAccountID"])){
+            throw new Exception("Inte inloggad");
+        }
+        if($input["accountid"] != $_SESSION["signedInAccountID"]){
+            throw new Exception("Inte inloggad");
+        }
+        
+        //Parses db settings from file
+        $settings = parse_ini_file("settings.ini", true);   
+        //Creates a connection
+        $mysql = $settings["mysql"];
+        $connection = new mysqli($mysql["host"], $mysql["username"], $mysql["password"], $mysql["dbname"]);
+        
+        $accountid = $input["accountid"];
+            
+        //Fetches project
+        $sql = "SELECT projectid, `name` FROM participation INNER JOIN project ON forprojectid = projectid WHERE foraccountid = $accountid";    
+        $result = $connection->query($sql);
+        $projects = [];
+        while($row = $result->fetch_assoc()){
+            $projects[] = $row;
+        }
+
+        $response = [
+            "status"=>true,
+            "message"=>"Projekt hämtat",
+            "projects"=>$projects
+        ];
+    }catch(Exception $exc){
+        $response = [
+            "status"=>false,
+            "message"=>$exc->getMessage()
+        ];
+    }
+    echo json_encode($response);
+?>
